321c8c64dc76f6b16412f72eec7b0df5437bf4bf
VolganBY <arin201018@gmail.com>
2022-03-26 15:43:47 +0300

    My project JS

---
+++ index.js
@@ -0,0 +1,76 @@
+function sum(a, b) {
+    const result = a + b
+    console.log(result);
+}
+
+sum(5,3);
+
+const arrowSub = (num1, num2) => {
+    const result = num1-num2;
+    console.log(result);
+}
+
+arrowSub(4, 1);
+
+const divFunction = function (num1, num2) {
+    const result = num1 / num2;
+    console.log(result);
+}
+
+divFunction (10, 2);
+
+const arrowDiv = (num1, num2) => {
+    const result = num1 * num2;
+    console.log(result);
+}
+
+arrowDiv(6, 4);
+
+const arrowRem = (num1, num2) => {
+    const result = num1 % num2;
+    console.log(result);
+}
+
+arrowRem(5, 2);
+
+const arrowMore = (num1, num2) => {
+    const result = num1 > num2;
+    console.log(result);
+}
+arrowMore (3,9);
+
+const arrowLess = (num1, num2) => {
+    const result = num1 < num2;
+    console.log(result);
+}
+
+arrowLess(1,2);
+
+const arrowMoe = (num1, num2) => {
+    const result = num1 >= num2;
+    console.log(result);
+}
+
+arrowMoe(10, 3);
+
+
+const arrowLoe = (num1, num2) => {
+    const result = num1 <= num2;
+    console.log(result);
+}
+
+arrowLoe(10, 9);
+
+const arrowEqu = (num1, num2) => {
+    const result = num1 === num2;
+    console.log(result);
+}
+
+arrowEqu(3,3);
+
+const arrowNqu = (num1, num2) => {
+    const result = num1 !== num2;
+    console.log(result);
+}
+
+arrowNqu(0,1);
