39fa8f781af8d03d6e572ab5c1799f65f614bd91
VolganBY <arin201018@gmail.com>
2022-03-27 16:35:38 +0300

    My project JS 2

--- index.js
+++ index.js
@@ -1,191 +1,171 @@
 function sum(a, b) {
-    const result = a + b
-    console.log(result);
+    const result = a + b;
+    return result;
 }
-
-sum(5,3);
 
 const arrowSub = (num1, num2) => {
-    const result = num1-num2;
-    console.log(result);
+    const result = num1 - num2;
+    return result;
 }
-
-arrowSub(4, 1);
 
 const divFunction = function (num1, num2) {
     const result = num1 / num2;
-    console.log(result);
+    return result;
 }
-
-divFunction (10, 2);
 
 const arrowDiv = (num1, num2) => {
     const result = num1 * num2;
-    console.log(result);
+    return result;
 }
-
-arrowDiv(6, 4);
 
 const arrowRem = (num1, num2) => {
     const result = num1 % num2;
-    console.log(result);
+    return result;
 }
-
-arrowRem(5, 2);
 
 const arrowMore = (num1, num2) => {
     const result = num1 > num2;
-    console.log(result);
+    return result;
 }
-arrowMore (3,9);
 
 const arrowLess = (num1, num2) => {
     const result = num1 < num2;
-    console.log(result);
+    return result;
 }
 
-arrowLess(1,2);
 
 const arrowMoe = (num1, num2) => {
     const result = num1 >= num2;
-    console.log(result);
+    return result;
 }
-
-arrowMoe(10, 3);
-
 
 const arrowLoe = (num1, num2) => {
     const result = num1 <= num2;
-    console.log(result);
+    return result;
 }
-
-arrowLoe(10, 9);
 
 const arrowEqu = (num1, num2) => {
     const result = num1 === num2;
-    console.log(result);
+    return result;
 }
 
-arrowEqu(3,3);
 
 const arrowNqu = (num1, num2) => {
     const result = num1 !== num2;
-    console.log(result);
+    return result;
 }
-
-arrowNqu(0,1);
 
 const SumFunction = function (num1, num2) {
     const result = num1 + num2;
-    console.log(result);
+    return result;
 }
 
 const arrowSum = (num1, num2) => {
-    const result = num1+num2;
-    console.log(result);
+    const result = num1 + num2;
+    return result;
 }
 
 function sub(a, b) {
-    const result = a - b
-    console.log(result);
+    const result = a - b;
+    return result;
 }
 
 const SubFunction = function (num1, num2) {
     const result = num1 - num2;
-    console.log(result);
+    return result;
 }
 
 function div(a, b) {
-    const result = a / b
-    console.log(result);
+    const result = a / b;
+    return result;
 }
 
-sum(5,3);
-
-const arrowDiv = (num1, num2) => {
-    const result = num1/num2;
-    console.log(result);
+const arrowDive = (num1, num2) => {
+    const result = num1 / num2;
+    return result;
 }
 
 function Div(a, b) {
-    const result = a * b
-    console.log(result);
+    const result = a * b;
+    return result;
 }
 
-const divFunction = function (num1, num2) {
+const diveFunction = function (num1, num2) {
     const result = num1 * num2;
-    console.log(result);
+    return result;
 }
 
 function Rem(a, b) {
-    const result = a % b
-    console.log(result);
+    const result = a % b;
+    return result;
 }
 
 const remFunction = function (num1, num2) {
     const result = num1 % num2;
-    console.log(result);
+    return result;
 }
 
 function More(a, b) {
-    const result = a > b
-    console.log(result);
+    const result = a > b;
+    return result;
 }
 
 const moreFunction = function (num1, num2) {
     const result = num1 > num2;
-    console.log(result);
+    return result;
 }
 
 function Less(a, b) {
-    const result = a < b
-    console.log(result);
+    const result = a < b;
+    return result;
 }
 
 const lessFunction = function (num1, num2) {
     const result = num1 < num2;
-    console.log(result);
+    return result;
 }
 
 function Moe(a, b) {
-    const result = a >= b
-    console.log(result);
+    const result = a >= b;
+    return result;
 }
 
 const moeFunction = function (num1, num2) {
     const result = num1 >= num2;
-    console.log(result);
+    return result;
 }
 
 function Loe(a, b) {
-    const result = a <= b
-    console.log(result);
+    const result = a <= b;
+    return result;
 }
 
 const loeFunction = function (num1, num2) {
     const result = num1 <= num2;
-    console.log(result);
+    return result;
 }
 
 function Equ(a, b) {
-    const result = a === b
-    console.log(result);
+    const result = a === b;
+    return result;
 }
 
 const equFunction = function (num1, num2) {
     const result = num1 === num2;
-    console.log(result);
+    return result;
 }
 
 function Nqu(a, b) {
-    const result = a !== b
-    console.log(result);
-}
+    const result = a !== b;
+    return result;
 
-const nquFunction = function (num1, num2) {
-    const result = num1 !== num2;
-    console.log(result);
-}
+    const nquFunction = function (num1, num2) {
+        const result = num1 !== num2;
+        return result;
+    }
+
+    console.log(sum(1, 3));
+
 
 
 
